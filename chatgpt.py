c380a30f16263902cbb27316dbb28d534b74a89d
Riccardo La Rosa <r.larosa@gmail.com>
2023-09-30 16:41:36 -0400

    initial version to use langchain on our docs site

--- chatgpt.py
+++ chatgpt.py
@@ -9,14 +9,24 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Chroma
 
 import constants
 
+class TextColor:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[44m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
+
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+PERSIST = True
 
 query = None
 if len(sys.argv) > 1:
@@ -28,25 +38,39 @@ if PERSIST and os.path.exists("persist"):
   index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
   #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
+  loader = DirectoryLoader("data",glob="*.txt")
   if PERSIST:
     index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
   else:
     index = VectorstoreIndexCreator().from_loaders([loader])
 
+template = """Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+Use three sentences maximum and keep the answer as concise as possible. 
+Always say "thanks for asking!" at the end of the answer. 
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+# chain = ConversationalRetrievalChain.from_llm(
+#   llm=ChatOpenAI(model="gpt-3.5-turbo"),
+#   retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+#   chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+# )
 chain = ConversationalRetrievalChain.from_llm(
   llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1})
 )
 
 chat_history = []
 while True:
   if not query:
-    query = input("Prompt: ")
+    query = input(f"{TextColor.BLUE}Prompt: {TextColor.RESET}")
   if query in ['quit', 'q', 'exit']:
     sys.exit()
   result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
+  print(f"{TextColor.CYAN}{ result['answer'] }{TextColor.RESET}")
 
   chat_history.append((query, result['answer']))
   query = None
